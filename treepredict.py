9d7a763946636c5be0440b0d9a0086223808be12
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2014-12-08 14:40:31 +0200

    DecisionNode class definition.

--- treepredict.py
+++ treepredict.py
@@ -14,3 +14,12 @@ my_data = [['slashdot', 'USA', 'yes', 18, 'None'],
            ['slashdot', 'UK', 'no', 21, 'None'],
            ['google', 'UK', 'yes', 18, 'Basic'],
            ['kiwitobes', 'France', 'yes', 19, 'Basic']]
+
+
+class DecisionNode(object):
+    def __init__(self, column=-1, true_value=None, results=None, true_child=None, false_child=None):
+        self.column = column  # which column of the data we are testing
+        self.true_value = true_value  # the value that the column must match to get a true result
+        self.results = results  # dict of results for this leaf. None except leaf nodes.
+        self.true_child = true_child  # ref to child DecisionNode if the test is true
+        self.false_child = false_child  # false child
